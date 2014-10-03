7e636564c26c9c6eb90ea1d31db0ad9801fc39b8
Pewh <25minutes.again@gmail.com>
2014-10-03 13:05:51 +0700

    Use emblemjs

--- app/routes/customers.js
+++ app/routes/customers.js
@@ -3,5 +3,14 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model: function() {
 		return this.store.find('customer');
+	},
+	actions: {
+		showCreateForm: function() {
+			console.log('tes');
+			this.transitionTo('customers.create');
+		},
+		showEditForm: function(customer) {
+			this.transitionTo('customers.edit', customer);
+		}
 	}
 });
--- app/views/navigation.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.View.extend({
-	tagName: 'header',
-	classNames: ['navbar', 'navbar-inverse', 'navbar-fixed-top'],
-	templateName: 'render/navigation'
-});
