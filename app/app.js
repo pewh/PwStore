d0fc082aec6136220ac594da39b88baad931b0a2
Pewh <25minutes.again@gmail.com>
2014-10-16 16:49:19 +0700

    Upgrade to ember-cli 0.1.1

--- app/app.js
+++ app/app.js
@@ -1,14 +1,16 @@
 import Ember from 'ember';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
+import config from './config/environment';
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 var App = Ember.Application.extend({
-  modulePrefix: 'pw-store', // TODO: loaded via config
+  modulePrefix: config.modulePrefix,
+  podModulePrefix: config.podModulePrefix,
   Resolver: Resolver
 });
 
-loadInitializers(App, 'pw-store');
+loadInitializers(App, config.modulePrefix);
 
 export default App;
--- app/router.js
+++ app/router.js
@@ -1,16 +1,17 @@
 import Ember from 'ember';
+import config from './config/environment';
 
 var Router = Ember.Router.extend({
-	location: PwStoreENV.locationType
+  location: config.locationType
 });
 
 Router.map(function() {
-	this.resource('customers', function() {
-		this.route('edit', { path: '/:id' });
-		this.route('create');
-	});
+  this.resource('customers', function() {
+    this.route('edit', { path: '/:id' });
+    this.route('create');
+  });
 
-	this.route('missing', { path: '/*path' });
+  this.route('missing', { path: '/*path' });
 });
 
 export default Router;
