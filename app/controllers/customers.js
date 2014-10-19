6cf02e77367f845f4d346e2904b3ebae440e7914
Pewh Mint <25minutes.again@gmail.com>
2014-10-19 17:22:09 +0700

    Fix customer form

--- app/controllers/customers.js
+++ app/controllers/customers.js
@@ -3,18 +3,25 @@ import Ember from 'ember';
 export default Ember.ArrayController.extend({
 	filter: '',
 
+	checkFilterMatch: function( model, string ) {
+		var regex = new RegExp(string, 'gi');
+
+		for ( var attr in model._data ) {
+			if ( model.get(attr).match(regex) ) {
+				return true;
+			}
+		}
+
+		return false;
+	},
+
 	filteredModel: function() {
-		var filter = this.get('filter');
-		var regexRule = new RegExp(filter, 'gi');
+		var filter = this.get( 'filter' );
 
-		var filtered = this.get('customer').filter(function( customer ) {
-			return customer.get('name').match(regexRule) ||
-						 customer.get('address').match(regexRule) ||
-						 customer.get('contact').match(regexRule);
-		});
-
-		return filtered;
-	}.property('filter'),
+		return this.get('model').filter( function( model ) {
+			return this.checkFilterMatch( model, filter );
+		}, this);
+	}.property('filter', 'model.@each'),
 
 	actions: {
 		showCreateForm: function() {
@@ -26,10 +33,5 @@ export default Ember.ArrayController.extend({
 		destroyRecord: function( record ) {
 			record.destroyRecord();
 		}
-  },
-
-	toolbar: [
-		Ember.Object.create({ title: 'Edit', type: 'primary' }),
-		Ember.Object.create({ title: 'Delete', type: 'danger' })
-	]
+  }
 });
--- app/models/customer.js
+++ app/models/customer.js
@@ -3,7 +3,8 @@ import DS from 'ember-data';
 var Customer = DS.Model.extend({
   name: DS.attr('string'),
   address: DS.attr('string'),
-  contact: DS.attr('string')
+  contact: DS.attr('string'),
+  isntValid: Em.computed.not('isValid')
 });
 
 Customer.reopenClass({
@@ -11,7 +12,10 @@ Customer.reopenClass({
 		{ id: 1, name: 'customer 1', address: 'adsad', contact: '23424' },
 		{ id: 2, name: 'customer 2', address: 'fsdf', contact: '23424' },
 		{ id: 3, name: 'customer 3', address: 'bdfggd', contact: '23424' }
-	]
+	],
+  validations: {
+    name: { presence: true }
+  }
 });
 
 export default Customer;
