ff0181f8c031d2d59ad9c468c7adc2aabe2caf6e
Pewh Mint <25minutes.again@gmail.com>
2014-10-23 21:34:45 +0700

    Modify row column

--- app/helpers/format-currency.js
+++ app/helpers/format-currency.js
@@ -1,8 +1,8 @@
 import Ember from 'ember';
 
 export function formatCurrency(input) {
-  if ( ! input ) return 0;
-  return "Rp" + input.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,')
-};
+  if ( ! input ) { return 0; }
+  return "Rp" + input.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,');
+}
 
 export default Ember.Handlebars.makeBoundHelper(formatCurrency);
