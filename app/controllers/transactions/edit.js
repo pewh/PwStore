92a6fa1ed68a12777cff536f311ad5359db7c1f5
Pewh Mint <25minutes.again@gmail.com>
2014-10-25 21:05:34 +0700

    Trying to break problem on  when update returned_at

--- app/controllers/transactions/edit.js
+++ app/controllers/transactions/edit.js
@@ -6,13 +6,13 @@ export default Ember.ObjectController.extend({
 			this.transitionToRoute('transactions');
 		},
 		itemReturned: function( detail ) {
-      window.a = this.get('model');
-      this.set('model.returned_at', new Date());
-      // this.set('model').save();
-      this.get('model').pushObject({});
-			// this.get('model').save().then(function() {
-			// 	this.transitionToRoute('transctions');
-			// }.bind(this));
+      detail.set('returned_at', new Date());
+      detail.save();
+      window.z = this;
+
+      // this.get('model').save().then(function() {
+			// 	console.log('berhasil')
+			// });
 		}
 	}
 });
--- app/helpers/format-datetime.js
+++ app/helpers/format-datetime.js
@@ -1,8 +1,13 @@
 import Ember from 'ember';
 
 export function formatDatetime(input) {
-  if ( ! input ) { return ''; }
-  return '%@/%@/%@'.fmt(input.getDate(), input.getMonth()+1, input.getFullYear());
+  if ( input ) {
+    return input;
+    debugger;
+    return input.getDate(); // '%@/%@/%@'.fmt(input.getDate(), input.getMonth()+1, input.getFullYear());
+  } else {
+    return '';
+  }
 }
 
 export default Ember.Handlebars.makeBoundHelper(formatDatetime);
