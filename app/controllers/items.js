67609663ba8727bc521ccc5014888ad5d97fba2f
Pewh Mint <25minutes.again@gmail.com>
2014-10-25 17:34:35 +0700

    Fix error on item

--- app/controllers/items.js
+++ app/controllers/items.js
@@ -2,9 +2,5 @@ import Ember from 'ember';
 import MasterPageController from '../mixins/master-page-controller';
 
 export default Ember.ArrayController.extend(MasterPageController, {
-  route: {
-    index: 'items',
-    create: 'items.create',
-    edit: 'items.edit'
-  }
+  modelName: 'item'
 });
