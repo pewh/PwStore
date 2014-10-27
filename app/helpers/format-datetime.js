42a0981f732bb80e19928cf20e00c82c38a05d52
Pewh <25minutes.again@gmail.com>
2014-10-27 11:26:50 +0700

    Convert  to Date on helper

--- app/helpers/format-datetime.js
+++ app/helpers/format-datetime.js
@@ -1,13 +1,12 @@
-import Ember from 'ember';
+	import Ember from 'ember';
 
-export function formatDatetime(input) {
-  if ( input ) {
-    return input;
-    debugger;
-    return input.getDate(); // '%@/%@/%@'.fmt(input.getDate(), input.getMonth()+1, input.getFullYear());
-  } else {
-    return '';
-  }
-}
+	export function formatDatetime(input) {
+	  if ( input ) {
+	    var date = new Date(input);
+	    return '%@/%@/%@'.fmt(date.getDate(), date.getMonth()+1, date.getFullYear());
+	  } else {
+	    return '';
+	  }
+	}
 
-export default Ember.Handlebars.makeBoundHelper(formatDatetime);
+	export default Ember.Handlebars.makeBoundHelper(formatDatetime);
