b0db7428c929883e4ccb17cfd118c019aac8f812
Pewh Mint <25minutes.again@gmail.com>
2014-10-30 07:02:00 +0700

    Sass

--- app/controllers/items/index.js
+++ app/controllers/items/index.js
@@ -1,8 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
-	needs: ['transaction_detail'],
-
 	qtyStatus: function() {
 		return [{
 			label: 'Tersedia',
@@ -11,7 +9,7 @@ export default Ember.ArrayController.extend({
 			label: 'Dipinjam',
 			value: this.get('model').filterBy('is_available', false).length
 		}];
-	}.property('model.@each.is_available', 'controllers.transaction_detail.returned_at'),
+	}.property('model.@each.is_available'),
 
 	availableQty: function() {
     	return this.get('model').filterBy('is_available', true).length;
